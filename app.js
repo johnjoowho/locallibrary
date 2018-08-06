d1808e41334822272acb1f8eab4e4ca12703b2ab
JohnJ <makewavesandsurf@gmail.com>
2018-08-06 13:50:56 -0700

    adding files

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
-
-
+var compression = require('compression');
+var helmet = require('helmet');
 var app = express();
 
 //Set up mongoose connection
@@ -29,6 +29,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
